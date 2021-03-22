dcff692723bc2f752b79f1eac8b4b99baea000ff
José Júnior Zago <josejuniorzago@MacBook-Pro-de-Jose.local>
2021-03-22 19:21:32 -0300

    Initial Commit

---
+++ js/principal.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    jQuery.support.cors = true;
+
+    $("#BtnEnviarEmail").click(function (){
+        var nome = $("#TxtNome").val();
+        var email = $("#TxtEmail").val();
+        var telefone = $("#TxtTelefone").val();
+        var mensagem = $("#TxtMensagem").val();
+        var url = "http://api.lojafacilonline.com.br/api/Comum/EnviarEmail";
+
+        var postData = { 
+        "de" : email, 
+        "nome" : nome, 
+        "para" : "josejuniorzago@gmail.com", 
+        "assunto" : "Contato pelo Site",
+        "mensagem" : mensagem
+        }
+
+        console.log(postData);
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: postData,
+            beforeSend: function(xhr){
+                xhr.setRequestHeader("Token", "objectsoftware123!");
+            },
+            success: function(){
+                alert("Mensagem enviada com sucesso! Nossa equipe entrará em contato com você em breve.");
+            },
+            error: function(data){
+                alert("Houve uma falha ao tentar enviar a mensagem. Sinto muito!");
+                console.log(data);
+            },
+            dataType: "jsonp"
+          });
+
+    });
+
+});
